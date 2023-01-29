9c6ad9775e4ba742c04116e6d46b9ce8c4ec9ae6
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-01-29 23:02:16 +0100

    updated models

--- models/User.js
+++ models/User.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true
+        },
+        confirmpassword: {
+            type: String,
+            required: true
         }
     }, { timestamps: true }
 )
